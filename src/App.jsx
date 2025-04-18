c776479268ced36443ff66bfcd7ac182706d4a0d
laissa-albuquerquer <laissaalbuquerque@gmail.com>
2025-04-18 12:20:08 -0300

    ajustes interface, criacao tela home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import PlacarPartida from './pages/placar-partida';
+import './styles/index.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/placar-partida" element={<PlacarPartida />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,81 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import '../styles/home-style/home-style.css';
+import logo from '../assets/placarizando-logo.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  // Definindo o estado para cada contador
-  const [blueCount, setBlueCount] = useState(0);
-  const [redCount, setRedCount] = useState(0);
+  const navigate = useNavigate();
 
-  // Função para aumentar o contador da div azul
-  const handleBlueClick = () => {
-    setBlueCount(blueCount + 1);
-  };
-
-  // Função para aumentar o contador da div vermelha
-  const handleRedClick = () => {
-    setRedCount(redCount + 1);
-  };
-
-  // Função para reiniciar os contadores
-  const resetCounters = () => {
-    setBlueCount(0);
-    setRedCount(0);
-  };
-
-  // Função para diminuir o contador da div azul
-  const decreaseBlueCount = () => {
-    if (blueCount > 0) {
-      setBlueCount(blueCount - 1);
-    }
-  };
-
-  // Função para diminuir o contador da div vermelha
-  const decreaseRedCount = () => {
-    if (redCount > 0) {
-      setRedCount(redCount - 1);
-    }
+  const handleClick = () => {
+    navigate('/placar-partida');
   };
 
   return (
-    <main className="w-full h-screen flex justify-center items-center">
-      <div className="flex flex-col md:flex-row w-full max-w-screen-lg justify-between items-center gap-4">
-
-        <div className="basis-1/2 bg-blue-500 text-white flex flex-col justify-center items-center h-52 cursor-pointer">
-          <div
-            className="flex justify-center items-center h-full w-full cursor-pointer"
-            onClick={handleBlueClick}
-          >
-            <span className="text-4xl">{blueCount}</span>
-          </div>
-
-          <button
-            className="bg-orange-500 text-white py-2 px-4 rounded mt-4"
-            onClick={decreaseBlueCount}
-          >
-            Diminuir Azul
-          </button>
-        </div>
-
-        <div
-          className="w-1.5 bg-gray-600 h-72 rounded-full"
-          onClick={resetCounters}
-        ></div>
-
-        <div className="basis-1/2 bg-red-500 text-white flex flex-col justify-center items-center h-52 cursor-pointer">
-          <div
-            className="flex justify-center items-center h-full w-full cursor-pointer"
-            onClick={handleRedClick}
-          >
-            <span className="text-4xl">{redCount}</span>
-          </div>
-
-          <button
-            className="bg-orange-500 text-white py-2 px-4 rounded mt-4"
-            onClick={decreaseRedCount}
-          >
-            Diminuir Vermelho
-          </button>
-        </div>
+    <div className="home">
+      <div className="home-content">
+        <img src={logo} alt="Logo do jogo" />
+        <button onClick={handleClick}>iniciar</button>
       </div>
-    </main>
+    </div>
   );
 }
