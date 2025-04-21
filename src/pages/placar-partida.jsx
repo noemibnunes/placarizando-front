9604dad388eb984677b360c0dd7bc4fcf09b43fb
noemibnunes <noemibezerra1@gmail.com>
2025-04-20 23:09:49 -0300

    feat: add a contagem de set e a pontuacao de cada set

--- src/pages/placar-partida.jsx
+++ src/pages/placar-partida.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
-import '../styles/placar-partida-style/placar-partida-style.css';
-import logoPlacar from '../assets/placar.svg';
+import React, { useState } from "react";
+import "../styles/placar-partida-style/placar-partida-style.css";
+import logoPlacar from "../assets/placar.svg";
 
 export default function PlacarPartida() {
   // Definindo o estado para cada contador
   const [blueCount, setBlueCount] = useState(0);
   const [redCount, setRedCount] = useState(0);
+
+  // set
+  const [blueSetCount, setBlueSetCount] = useState(0);
+  const [redSetCount, setRedSetCount] = useState(0);
+
+  // set - pontuacao
+  const [blueSetScores, setBlueSetScores] = useState([]);
+  const [redSetScores, setRedSetScores] = useState([]);
 
   // Função para aumentar o contador da div azul
   const handleBlueClick = () => {
@@ -37,6 +45,19 @@ export default function PlacarPartida() {
     }
   };
 
+  // Finalizar o set
+  const finalizarSet = () => {
+    if (blueCount > redCount) {
+      setBlueSetCount(blueSetCount + 1);
+      setBlueSetScores([...blueSetScores, blueCount]);
+    } else if (redCount > blueCount) {
+      setRedSetCount(redSetCount + 1);
+      setRedSetScores([...redSetScores, redCount]);
+    }
+    setBlueCount(0);
+    setRedCount(0);
+  };
+
   return (
     <main className="partida">
       <div className="titulo-container">
@@ -44,7 +65,29 @@ export default function PlacarPartida() {
           <img src={logoPlacar} alt="Logo do placar" />
           <span>Modo Partida</span>
         </div>
+      </div>
+
+      <div className="set-info">
         <h1>SET</h1>
+        <div className="set-contadores-completo">
+          <div className="set-lateral">
+            <span className="set-count azul">{blueSetCount}</span>
+            <ul className="score-list">
+              {blueSetScores.map((score, index) => (
+                <li key={index}>{score}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="set-lateral">
+            <span className="set-count vermelho">{redSetCount}</span>
+            <ul className="score-list">
+              {redSetScores.map((score, index) => (
+                <li key={index}>{score}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
 
       <div className="placares">
@@ -80,7 +123,7 @@ export default function PlacarPartida() {
           </button>
         </div>
       </div>
-      <button>Finalizar Set</button>
+      <button onClick={finalizarSet}>Finalizar Set</button>
     </main>
   );
 }
